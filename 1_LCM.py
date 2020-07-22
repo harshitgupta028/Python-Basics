0e1b1b18e76c4bc64edfdca2cb14b5e90a7b07f1
harshitg123 <harshit.28.sps@gmail.com>
2020-07-22 10:06:04 +0530

    LCM

---
+++ 1_LCM.py
@@ -0,0 +1,31 @@
+# Python program to calculate the LCM and HCF of more then two no's
+
+def find_lcm(a,b):
+    if a>b:
+        num=a
+        den=b
+    else:
+        num=b
+        den=a
+    rem=num%den
+    
+    while(rem!=0):
+        num=den
+        den=rem
+        rem=num%den
+    gcd=den    
+    
+    lcm= int(int(a*b)/int(gcd))    
+    return lcm
+
+l = [2, 7, 3, 9, 4] 
+
+a=l[0]
+b=l[1]
+
+lcm=find_lcm(a,b)
+
+for i in range(2,len(l)):
+    lcm=find_lcm(lcm,l[i])
+print(lcm)    
+    
